0bd720587e48f4b8dde53f841ab5d2a8a0e61f0b
Jordandiazfr <jordandiazfr@gmail.com>
2021-05-26 12:55:20 +0200

    Updates

--- src/components/Geoloc.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ComponentProps } from 'react'
-
-export default function Geoloc({props}: any) {
-    return (
-        <div>
-              <button className="btn-city" title="Use your geolocalization coords" onClick={ ()=> props.action() }> <i className="fas fa-map-marked-alt fa-2x icon"></i> </button>
-        </div>
-    )
-}
